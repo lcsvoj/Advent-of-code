0f534bd1a1018584222dc14ade551778c4f8153a
Lucas Vilela <lcsvoj@gmail.com>
2024-12-02 12:10:57 +0100

    2024 init

---
+++ 2024/01/HistorianHysteria.java
@@ -0,0 +1,13 @@
+package AdventOfCode;
+
+// Pair up the smallest number in the left list with the smallest number in the right list, then the second-smallest left number with the second-smallest right number, and so on.
+// Within each pair, figure out how far apart the two numbers are; you'll need to add up all of those distances. For example, if you pair up a 3 from the left list with a 7 from the right list, the distance apart is 4; if you pair up a 9 with a 3, the distance apart is 6.
+class HistorianHysteria {
+
+    public void main(String[] args) {
+
+    }
+
+    private int totalDistance()
+
+}
