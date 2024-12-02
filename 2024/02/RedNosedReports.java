9402e5120a86659ad1f52165255f4a93cdb9e6a5
Lucas Vilela <lcsvoj@gmail.com>
2024-12-02 16:08:42 +0100

    2024-02.2 done

--- 2024/02/RedNosedReports.java
+++ 2024/02/RedNosedReports.java
@@ -10,33 +10,37 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 class RedNosedReports {
 
-    private final List<List<String>> reports;
+    private final List<List<Integer>> reports;
 
     RedNosedReports(String fileName) {
         this.reports = getInput(fileName);
     }
 
-    private List<List<String>> getInput(String fileName) {
-        List<List<String>> r = new ArrayList<>();
+    private List<List<Integer>> getInput(String fileName) {
+        List<List<Integer>> rs = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(fileName))) {
             String report = null;
             while ((report = in.readLine()) != null) {
-                r.add(Arrays.asList(report.split("\\s+")));
+                String[] levelsStr = report.split("\\s+");
+                List<Integer> r = new ArrayList<>();
+                for (String v : levelsStr) {
+                    r.add(Integer.valueOf(v));
+                }
+                rs.add(r);
             }
         } catch (IOException e) {
             System.out.println("Invalid fileName");
         }
-        return r;
+        return rs;
     }
 
     private int countSafeReports() {
         int safeReports = 0;
-        for (List<String> report : reports) {
+        for (List<Integer> report : reports) {
             if (isSafe(report)) {
                 safeReports++;
             }
@@ -44,12 +48,30 @@ class RedNosedReports {
         return safeReports;
     }
 
-    private boolean isSafe(List<String> report) {
-        boolean shouldIncrease = Integer.valueOf(report.get(1)) - Integer.valueOf(report.get(0)) > 0;
+    private boolean isSafe(List<Integer> report) {
+        if (report.size() < 2) {
+            return false;
+        }
+        if (testReport(report)) {
+            return true;
+        } else {
+            for (int i = 0; i < report.size(); i++) {
+                List<Integer> newReport = new ArrayList<>(report);
+                newReport.remove(i);
+                if (testReport(newReport)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    private boolean testReport(List<Integer> report) {
+        boolean shouldIncrease = report.get(1) - report.get(0) > 0;
         for (int i = 1; i < report.size(); i++) {
 
             // The levels are either all increasing or all decreasing.
-            int dif = Integer.valueOf(report.get(i)) - Integer.valueOf(report.get(i - 1));
+            int dif = report.get(i) - report.get(i - 1);
             if (shouldIncrease && dif < 0 || !shouldIncrease && dif > 0) {
                 return false;
             }
